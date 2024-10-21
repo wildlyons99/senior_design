6a80a0a4a228d869c51ff4055fd87afef3d437cb
Tom Lyons <thomas.lyons@tufts.edu>
2024-10-21 19:38:09 -0400

    2 bit multiply - need waves

--- vhdl/matrix_mult/threeXthree_tb.vhd
+++ vhdl/matrix_mult/threeXthree_tb.vhd
@@ -12,21 +12,19 @@ component threeXthree is
     port(
         clk        : in  std_logic; 
         rst        : in  std_logic; 
-        -- 1 bit 3x3 vectors
-        matrixA    : in  unsigned(8 downto 0);
-        matrixB    : in  unsigned(8 downto 0);
-        
-        -- 2 bit 3x3 vectors to handle multiplication
-        matrixC    : out unsigned(8 downto 0)
+        -- 2 bit 3x3 vectors
+        matrixA    : in  unsigned(17 downto 0);
+        matrixB    : in  unsigned(17 downto 0);
+        matrixC    : out unsigned(17 downto 0)
     );
 end component;
 
-signal testA : unsigned(8 downto 0); 
-signal testB : unsigned(8 downto 0); 
-signal testC : unsigned(8 downto 0); 
+signal testA : unsigned(17 downto 0); 
+signal testB : unsigned(17 downto 0); 
+signal testC : unsigned(17 downto 0); 
 
-signal clk : std_logic; 
-signal rst : std_logic; 
+signal test_clk : std_logic; 
+signal test_rst : std_logic; 
 
 type matType is array(0 to 2, 0 to 2) of std_logic;
 signal matC : matType := (others => (others => '0')); 
@@ -34,30 +32,36 @@ signal matC : matType := (others => (others => '0'));
 begin
 
 matrix_multiplier : threeXthree port map 
-    (clk => test_clock, 
-     rst => test_reset, 
+    (clk => test_clk, 
+     rst => test_rst, 
      matrixA => testA, 
      matrixB => testB, 
      matrixC => testC
     );
 
 uut : process begin
-    reset <= '1';
-    wait for 1 ns;
+    test_rst <= '1';
+    wait for 10 ns;
 
-    reset <= '0';
-    wait for 1 ns;
+    test_rst <= '0';
+    test_clk <= '0'; 
+    wait for 10 ns;
 
-    testA <= "110100110";
-    testB <= "000110101";
-    wait for 1 ns;
-    -- assert testC <= "01" & "01" & "00" 
-                --   & "00" & "00" & "00" 
-                --   & "01" & "01" & "00" 
-    assert testC <= "1" & "1" & "0" 
-                  & "0" & "0" & "0" 
-                  & "1" & "1" & "0" 
+    testA <= "010100010000010100";
+    testB <= "000000010100010001";
+    wait for 10 ns; 
+
+    -- trigger rising edge
+    test_clk <= '1'; 
+    wait for 10 ns;
+
+    assert testC = "01" & "01" & "00" 
+                  & "00" & "00" & "00" 
+                  & "01" & "01" & "00" 
+ 
     report "Incorrect marix multiplication! :(";  
+
+    wait; 
 end process;
 
 end behav;
