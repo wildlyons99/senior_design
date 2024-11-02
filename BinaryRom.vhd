bf4e80ec1b85bc935b0ae67ec08df69cf00b26a3
corylemberg <corylemberg@gmail.com>
2024-11-02 14:13:33 -0400

    Update BinaryRom.vhd

--- BinaryRom.vhd
+++ BinaryRom.vhd
@@ -15,20 +15,20 @@ begin
         if rising_edge(clk) then
             case totaladr is
                 when "0000" => grayScale <= "00000000";
-                when "0001" => grayScale <= "00100101";
-                when "0010" => grayScale <= "00100010";
+                when "0001" => grayScale <= "00010010";
+                when "0010" => grayScale <= "00010001";
                 when "0011" => grayScale <= "00000000";
                 when "0100" => grayScale <= "00000000";
-                when "0101" => grayScale <= "01000110";
-                when "0110" => grayScale <= "01010000";
+                when "0101" => grayScale <= "00100011";
+                when "0110" => grayScale <= "00101000";
                 when "0111" => grayScale <= "00000000";
                 when "1000" => grayScale <= "00000000";
-                when "1001" => grayScale <= "01000001";
-                when "1010" => grayScale <= "01010100";
+                when "1001" => grayScale <= "00100000";
+                when "1010" => grayScale <= "00101010";
                 when "1011" => grayScale <= "00000000";
                 when "1100" => grayScale <= "00000000";
-                when "1101" => grayScale <= "00100111";
-                when "1110" => grayScale <= "00111010";
+                when "1101" => grayScale <= "00010011";
+                when "1110" => grayScale <= "00011101";
                 when "1111" => grayScale <= "00000000";
                 when others => grayScale <= "00000000";
         end case;
