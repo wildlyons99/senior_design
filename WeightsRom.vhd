cb2a168177310c3b4ddc3c5a1a09832f6d42a00f
corylemberg <corylemberg@gmail.com>
2024-11-08 13:52:22 -0500

    Add files via upload

--- WeightsRom.vhd
+++ WeightsRom.vhd
@@ -2,14 +2,12 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;entity WeightRom is    port(
         clk : in std_logic;
-        xadr: in unsigned(1 downto 0);
-        yadr : in unsigned(1 downto 0); -- 0-1023
+        totaladr: in unsigned(3 downto 0);
         weight : out signed(7 downto 0)
         );
 end WeightRom;
 
 architecture synth of WeightRom is
-signal totaladr : std_logic_vector(3 downto 0);
 begin
     process (clk) begin
         if rising_edge(clk) then
@@ -29,9 +27,9 @@ begin
                 when "1100" => weight <= "00100010";
                 when "1101" => weight <= "11001111";
                 when "1110" => weight <= "11101000";
+                when "1111" => weight <= "01001000";
                 when others => weight <= "00000000";
         end case;
     end if;
     end process;
-    totaladr <= std_logic_vector(yadr) & std_logic_vector(xadr);
 end;
