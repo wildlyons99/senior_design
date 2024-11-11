e804561a3831bb5131f252f06a02dc7b12c0e889
Tom Lyons <thomas.lyons@tufts.edu>
2024-11-11 16:25:27 -0500

    Git Reorginization

---
+++ perceptron_v1/fpga/multiplier_acc_v2/testbench/dut_params.v
@@ -0,0 +1,14 @@
+localparam ADD_SUB = "add";
+localparam A_WIDTH = 18;
+localparam B_WIDTH = 18;
+localparam ACC_WIDTH = 37;
+localparam A_SIGNED = "on";
+localparam B_SIGNED = "on";
+localparam USE_IREG = "off";
+localparam USE_OREG = "on";
+localparam PIPELINES = 1;
+localparam IMPL = "LUT";
+localparam FAMILY = "iCE40UP";
+`define iCE40UP
+`define ice40tp
+`define iCE40UP5K
