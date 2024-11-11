1222ad5e0bc54bcde1d5c18d3297230f477ee076
Kinan N. Rabbat <krabba01@vm-hw08.eecs.tufts.edu>
2024-11-11 16:43:32 -0500

    housekeeping on mult (kinan perceptron

--- perceptron_v1/kinan_perceptron/mult_tb.vhd
+++ perceptron_v1/kinan_perceptron/mult_tb.vhd
@@ -16,35 +16,49 @@ architecture structural of mult_tb is
     end component mult;
 
     signal clk : std_logic;
-    signal input_test : byte_array := (
-        x"FF", x"FF", x"FF", x"FF", x"FF", x"FF", x"FF", x"FF",
-        x"01", x"01", x"01", x"01", x"01", x"01", x"01", x"01"
+    signal input_data : byte_array := (
+        "00000000",
+        "00010010",
+        "00010001",
+        "00000000",
+        "00000000",
+        "00100011",
+        "00101000",
+        "00000000",
+        "00000000",
+        "00100000",
+        "00101010",
+        "00000000",
+        "00000000",
+        "00010011",
+        "00011101",
+        "00000000"
     );
-    signal output_test : std_logic_vector(15 downto 0) := x"0000";
 
-begin 
+    signal output_test : std_logic_vector(15 downto 0) := x"0000";
+    begin 
     uut : mult port map(
         clk => clk,
-        input => input_test,
+        input => input_data,
         output => output_test
     );
-    
-stimulus_process: process
-    begin   
 
-    for i in 0 to 18 loop
+    stimulus_process: process
+        begin 
+
+        for i in 0 to 18 loop
         clk <= '0';
         wait for 10 ns; 
         clk <= '1';
         wait for 10 ns; 
         report "Output value: " & integer'image(to_integer(signed(output_test))) severity note;
-    end loop;
+        end loop;
+
+        report "Final Output value: " & integer'image(to_integer(signed(output_test))) severity note;
 
 
-    
+        assert false report "Test done." severity note;
 
-assert false report "Test done." severity note;
-
-wait; -- wait indefinitely; otherwise this code loop
-end process;
+        wait; -- wait indefinitely; otherwise this code loop
+        end process;
 end structural;
