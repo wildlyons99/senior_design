85324df73a34f0a389b3dbfe6aa43cdc6d050717
Kinan N. Rabbat <krabba01@vm-hw08.eecs.tufts.edu>
2024-11-11 17:34:48 -0500

    Working dot product, tb

--- perceptron_v1/kinan_perceptron/mult.vhd
+++ perceptron_v1/kinan_perceptron/mult.vhd
@@ -14,42 +14,43 @@ end mult;
 
 architecture behavioral of mult is 
     signal count : integer := 0; 
-    signal temp_output : signed(15 downto 0) := (others => '0'); 
+    signal bias : signed(15 downto 0) := x"0800";
     signal weights : byte_array := (
-    "00111010",
-    "00011101",
-    "00010000",
-    "01010101",
-    "01001001",
-    "11101100",
-    "00010111",
-    "11010111",
-    "10111101",
-    "11101010",
-    "11111010",
-    "11000001",
-    "00100010",
-    "11001111",
-    "11101000",
-    "01001000"
+        "00111010",
+        "00011101",
+        "00010000",
+        "01010101",
+        "01001001",
+        "11101100",
+        "00010111",
+        "11010111",
+        "10111101",
+        "11101010",
+        "11111010",
+        "11000001",
+        "00100010",
+        "11001111",
+        "11101000",
+        "01001000"
     ); 
 
     begin 
         process(clk)
+        variable temp_output : signed(15 downto 0) := bias; 
         begin
         if rising_edge(clk) then 
-        if(count < 16) then
-        temp_output <= temp_output + signed(input(count)) * signed(weights(count));
-        output <= std_logic_vector(temp_output); 
-        count <= count + 1;
-        else
-        count <= 0;
-        output <= x"0000"; 
+            if(count < 16) then
+                temp_output := temp_output + signed(input(count)) * signed(weights(count));
+                output <= std_logic_vector(temp_output); 
+                count <= count + 1;
+            else
+                count <= 0;
+                output <= x"0000"; 
 
-        count <= 0;
-        temp_output <= (others => '0'); 
-        output <= std_logic_vector(temp_output); 
-        end if;
+                count <= 0;
+                temp_output := (others => '0'); 
+                output <= std_logic_vector(temp_output); 
+            end if;
         end if;
         end process;
 end behavioral;
--- perceptron_v1/kinan_perceptron/mult_tb.vhd
+++ perceptron_v1/kinan_perceptron/mult_tb.vhd
@@ -16,43 +16,65 @@ architecture structural of mult_tb is
     end component mult;
 
     signal clk : std_logic;
-    signal input_data : byte_array := (
+    signal zero_data : byte_array := (
         "00000000",
-        "00010010",
+        "00100101",
+        "00100010",
+        "00000000",
+        "00000000",
+        "01000110",
+        "01010000",
+        "00000000",
+        "00000000",
+        "01000001",
+        "01010100",
+        "00000000",
+        "00000000",
+        "00100111",
+        "00111010",
+        "00000000"
+    );
+
+    signal one_data : byte_array := (
+        "00000000",
+        "00000000",
+        "00011000",
+        "00000000",
+        "00000000",
+        "00000010",
+        "00111110",
+        "00000000",
+        "00000000",
+        "00110011",
         "00010001",
         "00000000",
         "00000000",
-        "00100011",
-        "00101000",
+        "00100001",
         "00000000",
-        "00000000",
-        "00100000",
-        "00101010",
-        "00000000",
-        "00000000",
-        "00010011",
-        "00011101",
         "00000000"
     );
+
 
     signal output_test : std_logic_vector(15 downto 0) := x"0000";
     begin 
     uut : mult port map(
         clk => clk,
-        input => input_data,
+        input => one_data,
         output => output_test
     );
 
     stimulus_process: process
         begin 
 
-        for i in 0 to 18 loop
-        clk <= '0';
-        wait for 10 ns; 
-        clk <= '1';
-        wait for 10 ns; 
-        report "Output value: " & integer'image(to_integer(signed(output_test))) severity note;
+        for i in 0 to 15 loop
+            clk <= '0';
+            wait for 10 ns; 
+            clk <= '1';
+            wait for 10 ns; 
+            -- report "Output value: " & integer'image(to_integer(signed(output_test))) severity note;
         end loop;
+
+
 
         report "Final Output value: " & integer'image(to_integer(signed(output_test))) severity note;
 
