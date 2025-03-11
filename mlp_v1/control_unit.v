db8ee0f4e3a94e4213639d206bcbd6781e68200c
Kinan Rabbat <116530375+kinanrabbat@users.noreply.github.com>
2025-03-10 21:57:52 -0400

    finished control unit

--- mlp_v1/control_unit.v
+++ mlp_v1/control_unit.v
@@ -2,8 +2,8 @@ module control_unit
     (   
         input clk, 
         input start,
-        output reg [5:0] input_neuron_addr,
-        output reg [10:0] output_neuron_addr,
+        output reg [11:0] input_neuron_addr,
+        output reg [11:0] output_neuron_addr,
         // 16 bits
         output reg [15:0] input_weight_addr,
         output reg reset_mult_acc,
@@ -17,7 +17,10 @@ module control_unit
     initial begin
         reset_mult_acc = 0;            
         write_neuron = 0;              
-        done = 0;      
+        done = 0;  
+        input_neuron_addr = 0;
+        output_neuron_addr = 0;
+        input_weight_addr = 0;
     end
     ////////////////////////////////////
 
@@ -38,9 +41,9 @@ module control_unit
 
 
     //////// variables ////////
-    reg [9:0] weight_ptr;
-    reg [3:0] neuron_ptr;
-    reg [1:0] layer_ptr;
+    reg [9:0]  weight_ptr;
+    reg [3:0]  neuron_ptr;
+    reg [1:0]  layer_ptr;
 
     initial begin
         weight_ptr = 10'b0;
@@ -50,47 +53,53 @@ module control_unit
     ///////////////////////////
 
 
-
-    //////// logic ////////
     always @(posedge clk) begin
         if (start && ~done) begin 
-            // done with curr neuron, move to next
-            if (weight_ptr == neuron_count_array[layer_ptr]) begin
-                neuron_ptr <= neuron_ptr + 1;
-                weight_ptr <= 8'h00;
+
+            //////// Set addresses /////////////////////////////////////
+            // (can be optimized with address bases)
+
+            // input neuron address = layer_ptr | weight_ptr, 12 bits 
+            input_neuron_addr <= {layer_ptr, weight_ptr};
+
+            // output neuron address = layer_ptr + 1| neuron_ptr, 12 bits
+            output_neuron_addr <= {(layer_ptr + 2'b01), 6'b000000, neuron_ptr};
+
+            // weight address = layer_ptr | neuron_ptr | weight_ptr, 16 bits
+            input_weight_addr <= {layer_ptr, neuron_ptr, weight_ptr};   
+                  
+            /////////////////////////////////////////////////////////////
+
+            // Last weight for current neuron, move to the next neuron 
+            if (weight_ptr == neuron_count_array[layer_ptr] - 1) begin
+        
+                // Last neuron in current layer, move to the next layer
+                if ((neuron_ptr + 1) == neuron_count_array[(layer_ptr + 1)]) begin
+
+                    // Finished MLP calculations, proceed to softmax
+                    if (neuron_count_array[(layer_ptr + 1)] == 1) begin 
+
+                    // if (layer_ptr == layers - 1 && neuron_ptr == neuron_count_array[layer_ptr] - 1) begin 
+                        done <= 1;
+                    end else begin 
+                        done <= 0;
+                        layer_ptr <= layer_ptr + 1;
+                        neuron_ptr <= 0;
+                    end
+                end else begin 
+                    neuron_ptr <= neuron_ptr + 1;
+                end
+
+                weight_ptr <= 0;
                 write_neuron <= 1;
-                reset_mult_acc <= 1;
+                reset_mult_acc <= 1; 
             end else begin
+                weight_ptr <= weight_ptr + 1;
                 write_neuron <= 0;
                 reset_mult_acc <= 0;
             end
 
-            // done with curr layer, move to next
-            if (neuron_ptr == neuron_count_array[layer_ptr]) begin
-                layer_ptr <= layer_ptr + 1;
-                neuron_ptr <= 8'h00;
-            end
-
-            // done w mlp calculations, do softmax
-            if (neuron_count_array[layer_ptr] == 1) begin
-                done <= 1;
-            end else begin 
-                done <= 0;
-            end
-
-        // set addresses      - can be optimized with address bases 
-
-        // neuron address = layer | neuron 
-        input_neuron_addr <= {layer_ptr, neuron_ptr};
-        output_neuron_addr <= {layer_ptr + 1, neuron_ptr};
-
-        input_weight_addr <= {layer_ptr, neuron_ptr, weight_ptr};
-
-        weight_ptr <= weight_ptr + 1;
-
         end 
+    end
 
-    ///////////////////////
-
-
-end module
+endmodule
